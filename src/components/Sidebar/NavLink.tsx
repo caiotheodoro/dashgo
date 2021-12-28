252328d23265c517a99f4b0d97e0df5b810aa6d1
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2021-12-28 17:25:14 -0300

    link active

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,6 +1,6 @@
 import { Link as ChakraLink, Icon, Text } from '@chakra-ui/react';
 import { ElementType } from 'react';
-import Link from 'next/link';
+import { ActiveLink } from '../ActiveLink';
 
 interface NavLinkProps {
     icon: ElementType;
@@ -9,11 +9,11 @@ interface NavLinkProps {
 }
 export function NavLink({ icon, href, children, ...rest }: NavLinkProps) {
     return (
-        <Link href={href}>
-        <ChakraLink display="flex" align="center" {...rest} >
-            <Icon as={icon} fontSize="20" />
-            <Text ml="4" fontWeight="medium">{children}</Text>
-        </ChakraLink>
-        </Link>
+        <ActiveLink href={href}>
+            <ChakraLink display="flex" align="center" {...rest} >
+                <Icon as={icon} fontSize="20" />
+                <Text ml="4" fontWeight="medium">{children}</Text>
+            </ChakraLink>
+        </ActiveLink>
     )
 }
